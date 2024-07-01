f5407638d856886735cbea26956170c4170e0fcf
Eustace831697 <eustace831697@gmail.com>
2024-07-01 17:04:54 +0800

    補上GetAll頁面

--- BookKeeping.Repository/Dtos/Invoice.cs
+++ BookKeeping.Repository/Dtos/Invoice.cs
@@ -9,42 +9,32 @@ using System.Threading.Tasks;
 namespace BookKeeping.Repository.Dtos
 {
     public class Invoice
-    {
-        //發票主檔的實體
-
+    {      
         public Guid ID { get; set; }
 
         [DisplayName("載具名稱")]
         public string Carrier_Name { get; set; }
 
-
         [DisplayName("載具號碼")]
         public string Carrier_Number { get; set; }
-
 
         [DisplayName("發票日期")]
         public DateTime Date { get; set; }
 
-
         [DisplayName("商店統編")]
         public string BAN_of_Seller { get; set; }
-
 
         [DisplayName("商店店名")]
         public string Name_of_Seller { get; set; }
 
-
         [DisplayName("發票號碼")]
         public string Invoice_Number { get; set; }
-
 
         [DisplayName("總金額")]
         public int Amount { get; set; }
 
-
         [DisplayName("發票狀態")]
         public string Invoice_Status { get; set; }
-
 
         [DisplayName("資料建立日期")]
         public DateTime CreateDate { get; set; }
