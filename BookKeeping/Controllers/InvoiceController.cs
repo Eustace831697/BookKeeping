b17b79fa678c436cf83b5a14370a565d28b615a8
Eustace831697 <eustace831697@gmail.com>
2024-07-08 16:24:16 +0800

    增加自訂Route

--- BookKeeping/Controllers/InvoiceController.cs
+++ BookKeeping/Controllers/InvoiceController.cs
@@ -17,21 +17,31 @@ namespace BookKeeping.Controllers
             this._invoiceService = invoiceService;
         }
 
-        //發票匯入頁面
-        public IActionResult Add()
+        [HttpGet("[Controller]/[action]")]
+        public IActionResult Create()
         {
             //取得明細分類列表
             var CategoryList = _invoiceService.getCategory();
 
             //組成SelectList傳至View
-            ViewBag.Category = new SelectList(CategoryList, "Value", "Text", 0);
+            ViewBag.CategoryList = new SelectList(CategoryList, "Value", "Text", 0);
 
             return View();
         }
 
-        //call Api儲存發票
-        [HttpPost("AddInvoice")]
-        public IActionResult AddInvoice(List<Invoice> Invoices)
+        [HttpGet("[controller]/{ID}")]
+        public IActionResult Edit(Guid ID)
+        {
+            Invoice model = _invoiceService.GetByID(ID)[0];
+
+            var CategoryList = _invoiceService.getCategory();
+            ViewBag.Category = new SelectList(CategoryList, "Value", "Text", 0);
+
+            return View(model);
+        }
+ 
+        [HttpPost("[controller]")]
+        public IActionResult Add(List<Invoice> Invoices)
         {
             string rtn = _invoiceService.Add(Invoices);
 
@@ -42,7 +52,30 @@ namespace BookKeeping.Controllers
             return Ok();
         }
 
-        //Add頁面讀取CSV檔案        
+        [HttpGet("[controller]")]
+        public IActionResult GetAll()
+        {
+            List<Invoice> model = _invoiceService.GetAll();
+
+            return View(model);
+        }
+
+        [HttpPut("[controller]")]
+        public IActionResult Update(Invoice invoice)
+        {
+            string rtn = _invoiceService.Update(invoice);
+
+            return Json(rtn);
+        }
+
+        [HttpDelete("[controller]")]
+        public IActionResult Delete(Guid ID)
+        {
+            string rtn = _invoiceService.Delete(ID);
+
+            return Json(rtn);
+        }
+
         [HttpPost("ReadFile")]
         public IActionResult ReadFile(IFormFileCollection files)
         {
@@ -50,42 +83,6 @@ namespace BookKeeping.Controllers
             List<Invoice> invoices = _invoiceService.ReadCSV(files);
 
             return Json(invoices);
-        }
-
-        //取得全部資料
-        public IActionResult GetAll()
-        {
-            List<Invoice> model = _invoiceService.GetAll();            
-
-            return View(model);
-        }
-
-        [HttpGet]
-        public IActionResult Edit(Guid ID)
-        {
-            Invoice model = _invoiceService.GetByID(ID)[0];
-
-            var CategoryList = _invoiceService.getCategory();
-            ViewBag.Category = new SelectList(CategoryList, "Value", "Text", 0);           
-
-            return View(model);
-        }
-
-        [HttpPost("UpdateInvoice")]
-        //更新資料的api
-        public IActionResult UpdateInvoice(Invoice invoice)
-        {
-            string rtn = _invoiceService.Update(invoice);
-
-            return Json(rtn); 
-        }
-
-        [HttpGet]
-        public IActionResult Delete(Guid ID)
-        {
-            string rtn = _invoiceService.Delete(ID);
-
-            return Json(rtn);
-        }
+        }        
     }
 }
