a74290134d0a08850303df069c2d17fed4dac390
Eustace831697 <eustace831697@gmail.com>
2024-07-20 19:49:34 +0800

    建立ErrorLog類別 主要紀錄Exception用

---
+++ BookingKeeping.Common/Implement/ErrorLog.cs
@@ -0,0 +1,57 @@
+﻿using BookingKeeping.Common.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BookingKeeping.Common.Implement
+{
+    public class ErrorLog : IErrorLog
+    {
+        private readonly string _LogPathRoot;
+
+        public ErrorLog(string LogPathRoot)
+        {
+            _LogPathRoot = LogPathRoot;
+        }
+
+        public void WriterLog(string Exception)
+        {
+            string LogContent = $"{DateTime.Now.ToString()}：\r\n{Exception}\r\n";
+
+            string LogFolderPath = GetFolderPath(_LogPathRoot);
+            string LogFilePath = GetFilePath(LogFolderPath);
+
+            File.AppendAllText(LogFilePath, LogContent);
+        }
+
+        private string GetFolderPath(string LogPath)
+        {
+            string FolderName = DateTime.Now.ToString("yyyyMM");
+            string FolderPath = Path.Combine(LogPath, FolderName);
+
+            if (!Directory.Exists(FolderPath))
+            {
+                Directory.CreateDirectory(FolderPath);
+            }
+
+            return FolderPath;
+        }
+
+        private string GetFilePath(string FolderPath)
+        {
+            string LogFileName = $"{DateTime.Now.ToString("yyyy-MM-dd")}.txt";
+            string LogFilePath = Path.Combine(FolderPath, LogFileName);
+
+            if (!File.Exists(LogFilePath))
+            {
+                File.Create(LogFilePath).Close();
+            }
+
+            return LogFilePath;
+        }
+    }
+}
